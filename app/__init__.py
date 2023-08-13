1f2b66fa99e846c1136b9d6707db6ad3952ed0d4
Chizy <chizy7@gmail.com>
2023-08-13 00:00:53 -0400

    Test

--- app/__init__.py
+++ app/__init__.py
@@ -117,5 +117,5 @@ def delete_time_line_post(id):
 
 # put app in debug mode
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000) # delete after test commit
     
